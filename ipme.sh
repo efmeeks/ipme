cbfd19337abba6c90d419087a7579980f9421401
Eric Meeks <eric@efmeeks.net>
2017-10-18 21:31:07 -0500

    .

--- ipme.sh
+++ ipme.sh
@@ -4,9 +4,9 @@
 # https://git.efmeeks.net/ipme
 
 ipme() {
-  echo ''; echo "  Public IP: $(wget -q -O - http://ipecho.net/plain)"
-  echo "  Private IP: $(ifconfig | egrep 'inet' | egrep -v 'inet6|127' | awk '{ print $2 }')"
-  echo -e "\x20 \x20 \xE2\x99\xA1 ipme"; echo ''
+  echo ''; echo "Public IP: $(wget -q -O - http://ipecho.net/plain)"
+  for ip in $(ifconfig | egrep 'inet' | egrep -v 'inet6|127' | awk '{ print $2 }'); do echo "Private IP: $ip"
+  echo -e "\x20\x20\xE2\x99\xA1 ipme"; echo ''
 } 
 
 ipme
